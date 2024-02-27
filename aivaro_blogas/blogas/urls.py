6c410e9e027670d2f665a33ba7d1cfe33c56ee68
aivarasmink <aivarasmink@gmail.com>
2024-02-27 11:09:41 +0200

    Mark task done/undone view, nepamirsti kad darau bloga ir viska perdaryti i bloga

--- aivaro_blogas/blogas/urls.py
+++ aivaro_blogas/blogas/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('tasks/', views.task_list, name='task_list'),
     path('task/<int:pk>/', views.task_detail, name='task_detail'),
+    path('task/<int:pk>/done/', views.task_done, name='task_done'),
 ]
 
