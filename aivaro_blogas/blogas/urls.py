f27a539b393523c8f676c90cd6f94baf166e28e8
aivarasmink <aivarasmink@gmail.com>
2024-02-27 10:46:20 +0200

    tasks list ir tasks ditail sukurta. kuriu fraima blogui, reiks viska perkeisti kad tiktu BLOGUI

--- aivaro_blogas/blogas/urls.py
+++ aivaro_blogas/blogas/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('tasks/', views.task_list, name='task_list'),
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
 ]
+
