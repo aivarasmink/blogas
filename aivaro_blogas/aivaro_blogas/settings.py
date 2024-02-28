e676e03c97644a5b6c5a7e5e0bc7a5f5fb557e61
aivarasmink <aivarasmink@gmail.com>
2024-02-28 08:24:40 +0200

    darau vertimus dar blogas neisvystytas nei truputi

--- aivaro_blogas/aivaro_blogas/settings.py
+++ aivaro_blogas/aivaro_blogas/settings.py
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -104,11 +105,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'lt'
+LANGUAGE_CODE = 'en-us'
+LANGUAGES = [
+    (LANGUAGE_CODE, 'English'),
+    ('lt', 'Lietuvių'),
+]
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Vilnius'
 
 USE_I18N = True
+USE_L10N = True
 
 USE_TZ = True
 
