fe465d0ffee1039d6bde6b0fa18a381aaf55a718
aivarasmink <aivarasmink@gmail.com>
2024-03-01 15:55:08 +0200

    uzstrigau su django shell      >>>import channels.layers

--- justchat/justchat/settings.py
+++ justchat/justchat/settings.py
@@ -115,3 +115,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Daphne
 ASGI_APPLICATION = "justchat.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
